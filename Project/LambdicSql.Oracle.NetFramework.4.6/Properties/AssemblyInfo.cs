c38ea0b0b287fe3cc2a1a2f3f05da70c2b513b9f
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-07-15 14:25:22 +0900

    separate .net 3.5 and 4.6.

---
+++ Project/LambdicSql.Oracle.NetFramework.4.6/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LambdicSql.Oracle.NetFramework.4.6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LambdicSql.Oracle.NetFramework.4.6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8461528b-81d2-4d62-b6d8-5ca94667f744")]
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
